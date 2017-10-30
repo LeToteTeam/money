8cc888b7a82b6c5892883b1004a38d8921a80c0c
Michael Elfassy <michael.elfassy@shopify.com>
2017-10-30 18:20:20 -0400

    money parser with currency support

--- lib/money/money_parser.rb
+++ lib/money/money_parser.rb
@@ -1,31 +1,41 @@
+# Parse an amount from a string
 class MoneyParser
-  # parse a amount from a string
+  MARKS = "\.,·’"
+  EXTRA_MARKS = "\s˙'"
+
   def self.parse(input, currency = nil)
     new.parse(input, currency)
   end
 
   def parse(input, currency = nil)
-    Money.new(extract_money(input.to_s), currency)
+    Money.new(extract_money(input.to_s, currency), currency)
   end
 
   private
-  def extract_money(input)
-    return Money.zero if input.to_s.empty?
 
-    amount = input.scan(/\-?[\d\.\,]+/).first
+  def extract_money(input, currency = nil)
+    return '0' if input.to_s.empty?
 
-    return Money.zero if amount.nil?
+    amount = input.scan(/(-?[\d#{MARKS}][\d#{MARKS}#{EXTRA_MARKS}]*)/).first
+    return '0' unless amount
+    amount = amount.first.tr(EXTRA_MARKS, '')
 
-    # Convert 0.123 or 0,123 into what will be parsed as a decimal amount 0.12 or 0.13
-    amount.gsub!(/^(-)?(0[,.]\d\d)\d+$/, '\1\2')
+    *other_marks, last_mark = amount.scan(/[#{MARKS}]/)
+    other_marks.uniq!
 
-    segments = amount.scan(/^(.*?)(?:[\.\,](\d{1,2}))?$/).first
+    return amount if last_mark.nil?
 
-    return Money.zero if segments.empty?
+    *dollars, cents = amount.split(last_mark)
+    dollars = dollars.join.tr(MARKS, '')
 
-    amount   = segments[0].gsub(/[^-\d]/, '')
-    decimals = segments[1].to_s.ljust(2, '0')
+    if cents.size > 2
+      c = Money::Helpers.value_to_currency(currency)
 
-    "#{amount}.#{decimals}"
+      if (!dollars.to_i.zero? && c.decimal_mark != last_mark) || other_marks == [last_mark]
+        return "#{dollars}#{cents}"
+      end
+    end
+
+    "#{dollars}.#{cents}"
   end
 end
